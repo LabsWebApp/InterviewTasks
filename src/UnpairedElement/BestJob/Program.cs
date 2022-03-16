340a111b6ae06e48b5f120e8a472eecb54400ec5
LabsWebApp <vl.skopin@gmail.com>
2022-03-16 17:23:27 +0700

    Добавлена задача №1 (поиск уникального числа в наборе чисел)

---
+++ src/UnpairedElement/BestJob/Program.cs
@@ -0,0 +1,7 @@
+﻿var source = new[] { 9, 88, -1, 7, -1, 1, 88, 7, 1, 7888, 9 };
+
+var result = 0;
+
+foreach (var element in source) result ^= element;
+
+Console.WriteLine(result);
---
+++ src/UnpairedElement/SortJob/Program.cs
@@ -0,0 +1,23 @@
+﻿var source = new[] { 9, 88, -1, 7, -1, 1, 88, 7, 1, 1000, 9 , 10, 10};
+
+//"один элемент непарный", значит как минимум он существует!
+if (source.Length == 1)
+{
+    Console.WriteLine(source[0]);
+    return;
+}
+
+//сложность сортировки o(n*ln2n), c# использует быструю сортировку(?)
+source = source.OrderBy(n => n).ToArray();
+
+//перебор вроде(?) не входит в сложность
+//"один элемент непарный", значит выйдет из цикла
+for (int i = 0;;)
+{
+    if (i + 1 == source.Length || source[i] != source[i + 1])
+    {
+        Console.WriteLine(source[i]);
+        return;
+    }
+    i += 2;
+}
