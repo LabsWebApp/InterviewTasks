387db02b01a53d74cc88d553bf926f77c980c902
LabsWebApp <vl.skopin@gmail.com>
2022-03-16 17:23:27 +0700

    Добавлена задача №1 (поиск уникального числа в наборе чисел)

---
+++ src/UnpairedElement/BestJob/Program.cs
@@ -0,0 +1,7 @@
+﻿var source = new[] { 9, 88, -1, 7, -1, 1, 88, 7, 1, 7888, 9 };
+
+//Сложность o(n)
+var result = 0;
+foreach (var element in source) result ^= element;
+
+Console.WriteLine(result);
---
+++ src/UnpairedElement/CountJob/Program.cs
@@ -0,0 +1,11 @@
+﻿var source = new[] { 9, 88, -1, 7, -1, 1, 88, 7, 1, 7888, 9 };
+
+//не могу оценить сложность - спросить у Н.В. Голковой !!!!!!!!!!!!!!!!!!!!
+foreach (var i in source)
+{
+    if (source.Count(e => e == i) == 1)
+    {
+        Console.WriteLine(i);
+        return;
+    }
+}
