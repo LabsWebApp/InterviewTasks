408bdaa174a6897981d2ff83be17128504b73f0f
LabsWebApp <vl.skopin@gmail.com>
2022-03-21 17:45:49 +0700

    Добавлена задача №3 (нахождение суммы простых чисел)

---
+++ src/PrimeNumber/BruteForceJob/Program.cs
@@ -0,0 +1,29 @@
+﻿const long end = 3000000L;
+
+Console.WriteLine(312471072265);
+
+bool IsPrime(long n)
+{
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    long i = 3;
+    while (i * i <= n)
+    {
+        if (n % i == 0) return false;
+        i += 2;
+    }
+
+    return true;
+}
+
+long result = 0;
+long i = 2;
+
+while (i <= end)
+{
+    if (IsPrime(i)) result += i;
+    i++;
+}
+
+Console.WriteLine(result);
---
+++ src/PrimeNumber/LinqJob/Program.cs
@@ -0,0 +1,46 @@
+﻿const int end = 3000000;
+
+Console.WriteLine(312471072265);
+
+// Обратите внимание!
+// Все проверки делались на int-перечислении
+// и только в конце подсчёт аккумулировался в long,
+// что существенно ускоряет и облегчает процесс.
+var result = StepRange(3, end - 1, i => i + 2)
+    .AsParallel()
+    .Where(n => Enumerable.Range(2, (int)Math.Sqrt(n))
+        .All(j => n % j != 0))
+    .Aggregate(2L, (x, y) => x + y);
+
+Console.WriteLine(result);
+
+static IEnumerable<int> StepRange(int start, int end, Func<int, int> step)
+{
+    while (start <= end)
+    {
+        yield return start;
+        start = step(start);
+    }
+}
+
+//Это переводилось на PLINQ:
+/*
+static IEnumerable<long> Primes()
+{
+    yield return 2;
+    for (int i = 3; i <= end; i += 2)
+    {
+        if (IsPrime(i))
+        {
+            yield return i;
+        }
+    }
+}
+
+static bool IsPrime(int number)
+{
+    for (int j = 3; j * j <= number; j += 2)
+        if (number % j == 0) return false;
+    return true;
+}
+*/
